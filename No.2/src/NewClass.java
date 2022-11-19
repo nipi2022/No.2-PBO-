c25c702b45bd457b46e2f41af6bf86dbcdceabc7
NIPI MASPUTRA <113754634+nipi2022@users.noreply.github.com>
2022-11-19 21:43:11 +0700

    Add files via upload

---
+++ No.2/src/NewClass.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ASUS PC
+ */
+public class NewClass {
+    
+    
+        public static void main (String args[]) {
+
+
+            System.out.println("==========================================");
+            System.out.println("Pendataan dan Pembayaran Iuran Warga");
+            System.out.println("==========================================");
+            System.out.println("1. Input Data Warga");
+            System.out.println("2. Input Iuran Warga");
+            System.out.println("PILIHAN MENU : 1");
+            System.out.println("------------------o0o-----------------------");
+            System.out.println("* Data Warga *");
+            System.out.println("Masukan Nama:");
+            System.out.println("Masukan NIK:");
+            System.out.println("Masukan Alamat:");
+            System.out.println("Masukan NoTlp:");
+            System.out.println("------------------o0o-----------------------");
+            System.out.println("* Data Pekerjaan *");
+            System.out.println("Masukan Pekerjaan : Marketing");
+            System.out.println("Masukan Alamat Kantor : Fatmawati");
+            System.out.println("------------------o0o-----------------------");
+            System.out.println("* Data Status *");
+            System.out.println("Masukan Status : Menikah");
+            System.out.println("Masukan Nama Suami/Istri : Sisilin");
+            System.out.println("Masukan Jumlah Anak : 2");
+
+            }
+        }
